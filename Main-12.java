101614e819fd039c578a4797adccc8027ad663c8
Bandar-AlOtaibi <93613167+Bandar-AlOtaibi@users.noreply.github.com>
2021-11-02 21:35:02 +0300

    Add files via upload

---
+++ Main-12.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(Math.sin(0));
+		System.out.println(Math.sin(Math.PI));
+		System.out.println(Math.sin(Math.PI/2));
+		System.out.println(Math.sin(Math.PI/6));
+		System.out.println(Math.cos(0));
+		;
+	}
+}
